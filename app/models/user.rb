22544938ca0d389b33a946625d4fc3fbdb1c8f8f
jacklynhma <jacklynhma@gmail.com>
2018-04-18 05:32:50 -0400

    delete commented method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
 class User < ApplicationRecord
   validates :stripe_customer_id, presence: true
-
-  # def create_stripe_customer(stripe_token)
-  #   customer = Stripe::Customer.create(
-  #     source: stripe_token
-  #   )
-  # end
 end
