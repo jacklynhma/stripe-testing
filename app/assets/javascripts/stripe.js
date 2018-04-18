42d4b1758972f76dffc9309b6aea595f10fafed9
jacklynhma <jacklynhma@gmail.com>
2018-04-18 05:24:11 -0400

    remove styling

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -5,27 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {// Create a Stripe cli
   // Create an instance of Elements
   var elements = stripe.elements();
 
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-  var style = {
-    base: {
-      color: '#32325d',
-      lineHeight: '18px',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '16px',
-      '::placeholder': {
-        color: '#aab7c4'
-      }
-    },
-    invalid: {
-      color: '#fa755a',
-      iconColor: '#fa755a'
-    }
-  };
-
   // Create an instance of the card Element
-  var card = elements.create('card', {style: style});
+  var card = elements.create('card');
 
   // Add an instance of the card Element into the `card-element` <div>
   card.mount('#card-element');
